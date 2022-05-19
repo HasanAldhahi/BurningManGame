19a519d07ac1304bb2e7ca4811d1d1aea159cfcb
Amjad <amjadmaqsuma@gmail.com>
2022-05-20 02:44:46 +0300

    Fixing Fetch API Along with the Hint Button

--- scripts/script.js
+++ scripts/script.js
@@ -4,14 +4,12 @@
 
 
 const btnContainer = document.getElementById("btn-container");
-console.log(btnContainer)
+
 
 const alpha = Array.from(Array(26)).map((e, i) => i + 65);
 const alphabet = alpha.map((x) => String.fromCharCode(x));
-console.log(alphabet);
-
 for ([i, letter] of Object.entries(alphabet)) {
-    btnContainer.innerHTML += `<button type="button" id="btn${i}" class="alph-btn col-lg-1 mx-2 my-3 col-md-4 btn btn-secondary fs-4" style= "width: 10%">${letter}</button>`
+    btnContainer.innerHTML += `<button type="button" id="btn${i}" name = "btns" class="alph-btn col-lg-1 mx-2 my-3 col-md-4 btn btn-secondary fs-4" style= "width: 10%">${letter}</button>`
 }
 
 
@@ -25,42 +23,27 @@ const btnHint = document.getElementById("btn-hint")
 
 
 
-
-
-fetch("https://random-word-api.herokuapp.com/word?number=1")
-    .then(word => word.json())
-    .then((randomWord) => {
-        const pressed = 0;
-        const getDefinition = () => {
-            fetch(`https://api.dictionaryapi.dev/api/v2/entries/en_US/${randomWord}`)
-                .then(response => {
-                    if (!response.ok) {
-                        throw "Sorry Pal, this is not a spelling Bee competition"
-                    }
-                    return response.json();
-                })
-                .then(arr => {
-                    console.log("hello")
-                    const def = arr[0]?.meanings[0].definitions["0"].definition;
-                    console.log(def);
-                    console.log(hint)
-                    hint.innerHTML += (`Hint: ${def}`)
-
-                }).catch(error => {
-                    console.log(error)
-                    hint.innerHTML += `${error}`
-                    console.log(hint)
-                })
+var word = []
+ const  fetchWord = () => { return fetch("https://random-word-api.herokuapp.com/word?number=1")
+    .then( word => word.json())
+    .then((randomWord) => {      
+        word = randomWord[0].split('')  
+        fetch(`https://api.dictionaryapi.dev/api/v2/entries/en_US/${randomWord}`)
+        .then(response => {if (!response.ok)  {
+            hint.innerHTML = (`No Available Hint for The current word, You are on your own now :(`)
+            throw "Sorry Pal, this is not a spelling Bee competition"
         }
-
-
-        btnHint.addEventListener('click', () => {
-            if (pressed !== 0) {
-                getDefinition();
-                pressed += 1;
-            }
+        return response.json()})
+        .then(arr => {
+            // console.log(arr)
+            const def = arr[0]?.meanings[0].definitions["0"].definition;
+            btnHint.addEventListener('click', () => {
+                 hint.innerHTML = (`Hint: ${def}`)
+        
+            })
+            
         })
-        return randomWord[0].split('');
-    })
 
+    })
+}
 
